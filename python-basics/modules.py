bed611e2f7a3ab7c2955d1d1a4742eef3175ceb0
Jon Banafato <jonathan.banafato@gmail.com>
2013-07-08 03:25:13 -0400

    modules

--- python-basics/modules.py
+++ python-basics/modules.py
@@ -1,10 +1,27 @@
-basic module usage
-__init__.py
+# importing modules
 
+# the following imports are all valid
 
-import x
-from x import y
-from x import *
-from x import y as z
+import os
+import os.path
+from os import path
+from os import path as p
+
+# you shouldn't do this. It will pollute your namespace, and it might not work
+# at all in certain cases. Always be expicit (see: import this).
+from datetime import *
+
+# the above is a warning here (and an error in python3):
+def myfunc():
+    from datetime import *
+    assert datetime
+    assert date
+
+# this will always fail (comment out this definition to get past it)
+def myfun2():
+    def inner():
+        from datetime import *
+        assert datetime
+        assert date
 
 import this
